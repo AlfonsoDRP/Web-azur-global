aef55379cf6a86c8fde6e406f2049302e1c5686b
evozkio <39371612+evozkio@users.noreply.github.com>
2022-05-10 14:13:10 +0200

    catalogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+
 
 @NgModule({
   declarations: [
+    CatalogoComponent,
     AppComponent
   ],
   imports: [
