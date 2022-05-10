037ca3faafc8e5a02c843fe2b1a29042f5218598
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-05-10 14:03:47 +0200

    contactanos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    CatalogoComponent,
+    SobreNosotrosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
