fcd9f10690db22884a4914af982f679f2c0788f1
Alfonso <alfonsoderojasperez@gmail.com>
2022-05-11 13:31:27 +0200

    Actualizacion

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,6 +11,25 @@ export class HomeComponent implements OnInit {
 scroll(){
   window.scroll(0,0);
 }
+  itemsPerSlide = 3;
+  singleSlideOffset = false;
+  noWrap = false;
+ 
+  slidesChangeMessage = '';
+ 
+  slides = [
+    {image: '../../../assets/piezas/AMORTIGUADORES.PNG',familia:"Amortiguadores"},
+    {image: '../../../assets/piezas/COMPRESOR.PNG',familia:"Compresores"},
+    {image: '../../../assets/piezas/FILTROS.PNG',familia:"Filtros"},
+    {image: '../../../assets/piezas/RODAMIENTOS.PNG',familia:"Rodamientos"},
+    {image: '../../../assets/piezas/espejo.png',familia:"Espejos"},
+    {image: '../../../assets/piezas/BATERIA.PNG',familia:"Baterias"},
+    
+  ];
+ 
+  onSlideRangeChange(indexes: number[]|void): void {
+    this.slidesChangeMessage = `Slides have been switched: ${indexes}`;
+  }
   ngOnInit(): void {
   }
 
