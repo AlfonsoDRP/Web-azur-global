fb0fa19fb1ae6bbff0c344be08a71c24bd050d5d
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-05-13 12:33:14 +0200

    font awesome footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAt, faEnvelope, faLocation, faLocationPin, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  faLocation=faLocationPin;
+  faEmail=faEnvelope;
+  faPhone=faPhone;
   constructor() { }
 
   ngOnInit(): void {
