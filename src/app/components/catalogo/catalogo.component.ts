e2541979d498363166339e3b7739f48f9106c2f1
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-05-18 13:33:18 +0200

    catalogo guai

--- src/app/components/catalogo/catalogo.component.ts
+++ src/app/components/catalogo/catalogo.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.scss']
 })
-export class CatalogoComponent implements OnInit {
+export class CatalogoComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ fotos:any=["../../../assets/marcas/LOGO-MOMOrecortada.png","../../../assets/marcas/LOGO-AJUSA.PNG", "../../../assets/marcas/LOGO-CRC.PNG","../../../assets/marcas/LOGO-JAPANPARTS.PNG","../../../assets/marcas/LOGO-KAVOPARTS.PNG","../../../assets/marcas/LOGO-PMM(2).png","../../../assets/marcas/LOGO-TRW.PNG","../../../assets/marcas/LOGO-MOMOrecortada.png","../../../assets/marcas/LOGO-AJUSA.PNG", "../../../assets/marcas/LOGO-CRC.PNG","../../../assets/marcas/LOGO-JAPANPARTS.PNG","../../../assets/marcas/LOGO-KAVOPARTS.PNG","../../../assets/marcas/LOGO-PMM(2).png","../../../assets/marcas/LOGO-TRW.PNG","../../../assets/marcas/LOGO-MOMOrecortada.png","../../../assets/marcas/LOGO-AJUSA.PNG", "../../../assets/marcas/LOGO-CRC.PNG","../../../assets/marcas/LOGO-JAPANPARTS.PNG","../../../assets/marcas/LOGO-KAVOPARTS.PNG","../../../assets/marcas/LOGO-PMM(2).png","../../../assets/marcas/LOGO-TRW.PNG"   ]
 
 }
