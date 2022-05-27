bb1f75506bac6a652cd0d5676aa351fdee7f174e
evozkio <39371612+evozkio@users.noreply.github.com>
2022-05-27 14:03:07 +0200

    animacion sobre nosotros

--- src/app/components/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/components/sobre-nosotros/sobre-nosotros.component.ts
@@ -16,6 +16,68 @@ export class SobreNosotrosComponent  {
   }
 
   actionMenu(numero:number){
-    this.numero_seccion = numero;
+    
+    let historia = document.getElementById('historia');
+    let valores = document.getElementById('valores');
+    let mision = document.getElementById('mision');
+
+    if (valores && mision && historia  && numero == 0 && this.numero_seccion != numero){
+      historia.style.left = "-30vw"
+      valores.style.left = "30vw"
+      mision.style.left = "30vw"
+      historia.style.opacity = "0"
+      valores.style.opacity = "0"
+      mision.style.opacity = "0"
+      setTimeout(() => {
+        this.numero_seccion = numero;
+      }, 500);
+      setTimeout(() => {
+        if (historia) {
+          historia.style.left = "0"
+          historia.style.opacity = "1.0"
+        }
+      }, 600);
+      
+    }
+    if (valores && mision && historia  && numero == 1 && this.numero_seccion != numero){
+      historia.style.left = "30vw"
+      valores.style.left = "-30vw"
+      mision.style.left = "30vw"
+      historia.style.opacity = "0"
+      valores.style.opacity = "0"
+      mision.style.opacity = "0"
+ 
+      setTimeout(() => {
+        this.numero_seccion = numero;
+      }, 500);
+      setTimeout(() => {
+        this.numero_seccion = numero;
+        if (valores) {
+          valores.style.left = "0"
+          valores.style.opacity = "1.0"
+        }
+      }, 600);
+
+    }
+    if (valores && mision && historia  && numero == 2 && this.numero_seccion != numero){
+      historia.style.left = "30vw"
+      valores.style.left = "30vw"
+      mision.style.left = "-30vw"
+      historia.style.opacity = "0"
+      valores.style.opacity = "0"
+      mision.style.opacity = "0"
+      setTimeout(() => {
+        this.numero_seccion = numero;
+      }, 500);
+      setTimeout(() => {
+        this.numero_seccion = numero;
+        if (mision) {
+          mision.style.left = "0"
+          mision.style.opacity = "1.0"
+        }
+      }, 600);
+
+
+    }
   }
 }
