422e6327379d3137e3e014ab9ccf5df85ed51e59
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-06-03 12:10:50 +0200

    componente boton estiloso

--- src/app/components/boton/boton.component.ts
+++ src/app/components/boton/boton.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-boton',
   templateUrl: './boton.component.html',
   styleUrls: ['./boton.component.scss']
 })
-export class BotonComponent implements OnInit {
+export class BotonComponent  {
  @Input() router:any;
  @Input() contenido:any;
- 
-  constructor() { }
-  
-  ngOnInit(): void {
-  }
+ @Input() estilo:string="basico";
+ @Input() size:string= '100px';
+
+
+
+ cambiarEstilo():string{
+   return this.estilo;
+ }
+
 
 }
