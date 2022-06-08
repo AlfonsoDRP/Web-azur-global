73d25ac8667abaa3994d61eaa7e57830f6f16a90
Alfonso <alfonsoderojasperez@gmail.com>
2022-06-08 09:40:45 +0200

    Actualizacion

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -38,10 +38,18 @@ export class HomeComponent {
     console.log(posScroll)
     // let derecha = document.getElementById("derecha");
     // let izquierda = document.getElementById("izquierda");
-    let primero = document.getElementById("primero");
-    let segundo = document.getElementById("segundo");
-    let tercero = document.getElementById("tercero");
-    let cuarto = document.getElementById("cuarto");
+    let cont1 = document.getElementById("contuno");
+    let cont2 = document.getElementById("contdos");
+    let cont3 = document.getElementById("conttres");
+    let cont4 = document.getElementById("contcuatro");
+    let primero = document.getElementById("texto-primero");
+    let segundo = document.getElementById("texto-segundo");
+    let tercero = document.getElementById("texto-tercero");
+    let cuarto = document.getElementById("texto-cuarto");
+    let imagen1 = document.getElementById("img-primero");
+    let imagen2 = document.getElementById("img-segundo");
+    let imagen3 = document.getElementById("img-tercero");
+    let imagen4 = document.getElementById("img-cuarto");
     
 
     // if (posScroll > 1100 && derecha && izquierda){
@@ -56,9 +64,11 @@ export class HomeComponent {
     //   izquierda.style.opacity = "0";
     // }
 
-    if (posScroll > 1000 && primero){
-      primero.style.right = "0";
-      primero.style.opacity = "1";
+    if (posScroll > 1000 && primero && imagen1 && cont1){
+      primero.style.bottom = "0";
+      primero.style.opacity = "1"
+      cont1.style.opacity = "1";
+      imagen1.style.transform = "scale(1.3)";
       this.bandera1 = true;
       }
     // }else if(posScroll < 1000 && primero){
@@ -66,27 +76,34 @@ export class HomeComponent {
     //   primero.style.opacity = "0";
     // }
 
-    if (posScroll > 1500 && segundo){
-      segundo.style.left = "0";
+    if (posScroll > 1500 && segundo && imagen2 && cont2){
+      segundo.style.bottom = "0";
       segundo.style.opacity = "1";
-      this.bandera2 = true;}
+      cont2.style.opacity = "1";
+      imagen2.style.transform = "scale(1.3)";
+      this.bandera2 = true;
+    }
     // }else if(posScroll < 1500 && segundo){
     //   segundo.style.left = "-100px";
     //   segundo.style.opacity = "0";
     // }
 
-    if (posScroll > 2000 && tercero){
-      tercero.style.right = "0";
+    if (posScroll > 2000 && tercero && imagen3 && cont3){
+      tercero.style.bottom = "0";
       tercero.style.opacity = "1";
+      cont3.style.opacity = "1";
+      imagen3.style.transform = "scale(1.3)";
       this.bandera3 = true;}
     // }else if(posScroll < 2000 && tercero){
     //   tercero.style.right = "-100px";
     //   tercero.style.opacity = "0";
     // }
 
-    if (posScroll > 2500 && cuarto){
-      cuarto.style.left = "0";
+    if (posScroll > 2500 && cuarto && imagen4 && cont4){
+      cuarto.style.bottom = "0";
       cuarto.style.opacity = "1";
+      cont4.style.opacity = "1";
+      imagen4.style.transform = "scale(1.3)";
       this.bandera4 = true;}
     // }else if(posScroll<2500 && cuarto){
     //   cuarto.style.left = "-100px";
