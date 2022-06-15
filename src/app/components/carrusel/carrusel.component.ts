8853c6ed66def41cd74bd3062bc7217e06ecade6
evozkio <39371612+evozkio@users.noreply.github.com>
2022-06-15 09:37:25 +0200

    carrusel

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -7,70 +7,105 @@ import {faCaretLeft, faCaretRight} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./carrusel.component.scss'],
 })
 export class CarruselComponent implements OnInit {
+
+
   ngOnInit(): void {
     let elemento = this.lista[this.lista.length - 1];
-    this.lista.push(this.lista[0]);
-    this.lista.unshift(elemento);
     this.elementos = this.lista.length;
-    this.centro = this.movil ? 0 : 1;
+
+    for (let i = 0; i < this.elementos; i++) {
+      this.listaIds.push('elemento' + i);
+    }
+    setTimeout(() => {
+      this.recalcularUbicacion(true);
+    }, 0);
   }
+
   @Input() lista = [
     {
       image: '../../../assets/piezas/AMORTIGUADORES.PNG',
       familia: 'Amortiguadores',
     },
+    {
+      image: '../../../assets/piezas/AMORTIGUADORES.PNG',
+      familia: 'Amortiguadores',
+    },
+    { image: '../../../assets/piezas/COMPRESOR.PNG', familia: 'Compresores' },
     { image: '../../../assets/piezas/COMPRESOR.PNG', familia: 'Compresores' },
     { image: '../../../assets/piezas/FILTROS.PNG', familia: 'Filtros' },
+    { image: '../../../assets/piezas/FILTROS.PNG', familia: 'Filtros' },
+    { image: '../../../assets/piezas/RODAMIENTOS.PNG', familia: 'Rodamientos' },
     { image: '../../../assets/piezas/RODAMIENTOS.PNG', familia: 'Rodamientos' },
   ];
   @Input() movil: boolean = false;
+  @Input() ancho: string = "0";
+  @Input() alto: string = "0";
+  @Input() colorFondo: string = "";
+  @Input() colorBoton: string = "";
+  posicion: number = 0;
+  centro: number = this.movil ? 1 : 2;
+  listaIds: Array<string> = [];
+  elementos = this.lista.length;
+  movimientoActivo: boolean = false;
   iconoLeft = faCaretLeft;
   iconoRight = faCaretRight;
-  posicion: number = 0;
-  centro: number = this.movil ? 0 : 1;
 
   @HostListener('window:resize', ['$event']) onResize(event: any) {
-    this.recaocularUbicacion();
+    this.recalcularUbicacion(true);
+  }
+  
+  dimesionesPrincipal():string {
+    return "width: "+this.ancho+"; height: "+this.alto;
   }
 
-  elementos = this.lista.length;
-
-  anchoTodo(): string {
-    return this.movil
-      ? 'width: calc(100% * ' + this.elementos + ');'
-      : 'width: calc(100% * ' + this.elementos + ' / 3);';
-  }
   anchoCaja(): string {
-    return 'width: calc(100% / ' + this.elementos + ' );';
+    return this.movil ?'width: calc(33% );' : 'width: calc(100% / ' + 5 + ' );';
   }
 
   animacion(valor: boolean) {
-    if (valor) {
-      if (this.posicion == 0) {
-        this.posicion = -1 * this.elementos + 3;
-        this.centro = this.elementos - 1;
+    if (!this.movimientoActivo) {
+      this.movimientoActivo = true;
+
+      if (valor) {
+        this.centro = this.centro + 1 >= this.elementos ? 0 : this.centro + 1;
       } else {
-        this.posicion = this.posicion + 1;
+        this.centro = this.centro - 1 < 0 ? this.elementos - 1 : this.centro - 1;
       }
-    } else {
-      if (this.posicion == -1 * this.elementos + 3) {
-        this.posicion = 0;
-        this.centro = this.posicion;
-      } else {
-        this.posicion = this.posicion - 1;
-      }
+
+      this.recalcularUbicacion(valor);
+
+      setTimeout(() => {
+        this.movimientoActivo = false;
+      }, 300);
     }
-    this.recaocularUbicacion();
   }
 
-  recaocularUbicacion() {
-    let carrusel = document.getElementById('contenedor-carrusel');
-    this.centro = this.movil ? this.posicion * -1 : this.posicion * -1 + 1;
-    let calculo = this.movil
-      ? (100 / this.elementos) * this.posicion * this.elementos
-      : ((100 / this.elementos) * this.posicion * this.elementos) / 3;
-    if (carrusel) {
-      carrusel.style.left = calculo + '%';
+  recalcularUbicacion(valor: boolean) {
+    console.log("hola")
+    let numero = [
+      this.centro > 0 ? this.centro - 2 : this.centro == 0 ? this.elementos - 2 : this.elementos - 1,
+      this.centro == 0 ? this.elementos - 1 : this.centro - 1, 
+      this.centro, 
+      this.centro + 1 < this.elementos ? this.centro + 1 : 0,
+      this.centro + 2 < this.elementos ? this.centro + 2 : this.centro + 2 == this.elementos ? 0 : 1,
+    ];
+
+    for (let i = 0; i < this.elementos; i++) {
+      let posicion = numero.indexOf(i);
+      let elemento = document.getElementById('elemento' + i);
+      if(this.movil){
+      
+        if (elemento) {
+          elemento.style.visibility = (posicion == 2 || posicion == 1 || posicion == 3) ? 'visible' : 'hidden';
+          elemento.style.left = 33 * (posicion-1) + '%';
+        }
+      }else{
+
+        if (elemento) {
+          elemento.style.visibility = posicion == 4 && valor ? 'hidden' : posicion == -1 && !valor ? 'hidden' : 'visible';
+          elemento.style.left = 20 * posicion + '%';
+        }
+      }
     }
   }
 }
